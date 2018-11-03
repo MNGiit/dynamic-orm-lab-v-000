3b587e618b8a5e47bd02a5c159581f90135401b4
Mohammed Noori <kmanoorib@gmail.com>
2018-11-03 02:47:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,4 +14,9 @@ class Student < InteractiveRecord #Student inherits from InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+  
+  def table_name_for_insert
+    
+  end
+  
 end
